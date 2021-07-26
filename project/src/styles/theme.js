cbb50a3a42b5edb567f37fc64f3faa5550f4aeab
lilly9928 <50159938+lilly9928@users.noreply.github.com>
2021-07-26 18:13:10 +0900

    Update theme.js

---
+++ project/src/styles/theme.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const container = styled.div`
+flex: 1,
+flexDirection: "column",
+`;
+
+const headContainer = styled.div`
+flex: 0.30,
+`;
+
+// const container = styled.div`
+// flex: 1,
+// flexDirection: "column",
+// `;
+
+
+// const container = styled.div`
+// flex: 1,
+// flexDirection: "column",
+// `;
+
+// const container = styled.div`
+// flex: 1,
+// flexDirection: "column",
+// `;
+
+// const container = styled.div`
+// flex: 1,
+// flexDirection: "column",
+// `;
+
+// const container = styled.div`
+// flex: 1,
+// flexDirection: "column",
+// `;
+
+// const container = styled.div`
+// flex: 1,
+// flexDirection: "column",
+// `;
+
+// const container = styled.div`
+// flex: 1,
+// flexDirection: "column",
+// `;
+
+
