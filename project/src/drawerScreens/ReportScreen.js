ee3529348cce4a822f6f6b776e4cc9e390d6110e
lilly9928 <50159938+lilly9928@users.noreply.github.com>
2021-10-13 19:01:39 +0900

    리포트

--- project/src/drawerScreens/ReportScreen.js
+++ project/src/drawerScreens/ReportScreen.js
@@ -25,12 +25,12 @@ import {
 function ReportScreen({ navigation }) {
 
   const data = Perference.getReportData();
-  const HeadTable= ['일시', '수익', '누적 수익', '살제 누적 수익'];
+  const HeadTable= ['일시', '수익', '누적 수익', '살제누적 수익'];
   const DataTable= Perference.getReportDataTable();
   console.log(DataTable);
   console.log(data);
   const date = new Date();
-  const dataDate = date.getFullYear()+'.'+date.getMonth()+'.'+date.getDate()+'.'+date.getHours()+':'+date.getMinutes()+'기준'
+  const dataDate = date.getHours()+':'+date.getMinutes()+'기준'
   const indexToClipFrom = date.getMonth();
   const money = Perference.getMoney();
 
@@ -48,8 +48,8 @@ function ReportScreen({ navigation }) {
     <Path
         key={ 'line-1' }
         d={ line }
-        stroke={ '#FFBF00' }
-        strokeWidth={ 2 }
+        stroke={ '#ffb851' }
+        strokeWidth={ 4 }
         fill={ 'none' }
         //strokeDasharray={ [ 4, 4 ] }
         clipPath={ 'url(#clip-path-2)' }
@@ -62,9 +62,9 @@ const HorizontalLine = (({ y }) => (
       x2={ '100%' }
       y1={ y(money) }
       y2={ y(money) }
-      stroke={ 'gray' }
+      stroke={ '#292929' }
      // strokeDasharray={ [ 4, 8 ] }
-      strokeWidth={ 2 }
+      strokeWidth={ 3 }
   />
 ))
 
@@ -75,8 +75,8 @@ const VerticalLine = (({ x }) => (
       x2={ x(6.5)}
       y1={ "0%" }
       y2={ "100%" }
-      stroke={ '#BE81F7' }
-      strokeWidth={ 2 }
+      stroke={ '#e051ff' }
+      strokeWidth={ 3 }
   />
 ))
 
@@ -85,61 +85,56 @@ const VerticalLine = (({ x }) => (
     
     <SafeAreaView style={styles.container}>
       <ScrollView>
+      <View style={styles.topContainerWrap}>
       <View style={styles.topContainer}>
-        <View style={styles.Box}>
-          <View style={styles.topBox}>
+        <View style={styles.borderBox}>
             <Text style={styles.Boxtitle}>이번달 예상 수익</Text>
-            <Text style={styles.Boxsubtitle}>{dataDate}</Text>
-          </View>
-          <View style={styles.middleBox}>
-            <Text style={styles.middlePriceText}>{Perference.getReportMonthPredicted()+'원'}</Text>
+            <Text style={styles.middlePriceTextYellow}>{Perference.getReportMonthPredicted()+'원'}</Text>
+            <View style={styles.insideMiddleBoxWrap}>
             <View style={styles.insideMiddleBox}>
-              <Text style={styles.middleText}>월 평균 비교: </Text>
-              <Text style={styles.middleText}>작년 1월 비교: </Text>
+              <Text style={styles.bordersubText}>월 평균 비교: </Text>
+              <Text style={styles.borderText}>{Perference.getReportMonthAverage()+'원'}</Text>
             </View>
             <View style={styles.insideMiddleBox}>
-              <Text style={styles.middleText}>{Perference.getReportMonthAverage()+'원'}</Text>
-              <Text style={styles.middleText}>{Perference.getReportLastYearOfMonth()+'원'}</Text>
+            <Text style={styles.bordersubText}>작년 1월 비교: </Text>
+              <Text style={styles.borderText}>{Perference.getReportLastYearOfMonth()+'원'}</Text>
             </View>
-          </View>
+            </View>
         </View>
-      </View>
-      <View style={styles.topContainer}>
-        <View style={styles.Box}>
-          <View style={styles.topBox}>
+        <View style={styles.borderBox}>
             <Text style={styles.Boxtitle}>현재까지 누적 수익</Text>
-            <Text style={styles.Boxsubtitle}>{dataDate}</Text>
-          </View>
-          <View style={styles.middleBox}>
-            <Text style={styles.middlePriceText}>{Perference.getReportTotalRevenue()+'원'}</Text>
+            <Text style={styles.middlePriceTextBlue}>{Perference.getReportTotalRevenue()+'원'}</Text>
+            <View style={styles.insideMiddleBoxWrap}>
             <View style={styles.insideMiddleBox}>
-              <Text style={styles.middleText}>투자원금: </Text>
-              <Text style={styles.middleText}>실제 누적 수익:</Text>
-              <Text style={styles.middleText}>(투자 수익 - 원금)</Text>
+              <Text style={styles.bordersubText}>투자원금: </Text>
+              <Text style={styles.borderText}>{Perference.getMoney()+'원'}</Text>
             </View>
             <View style={styles.insideMiddleBox}>
-              <Text style={styles.middleText}> {Perference.getMoney()+'원'}</Text>
-              <Text style={styles.middleText}> {Perference.getReportActualRevenue()+'원'}</Text>
+            <Text style={styles.bordersubText}>실제 누적 수익: </Text>
+              <Text style={styles.borderText}>{Perference.getReportActualRevenue()+'원'}</Text>
             </View>
-          </View>
+            </View>
         </View>
       </View>
-      <View style={styles.middleContainer}>
-        <View style={styles.Center}>
-          <Text style={styles.middleConText}>개월 후(*년 *월) 손익 분기점에 도달할 것으로 예상됩니다</Text>
-        </View>
-        <View style={styles.Box}>
-          <View style={styles.topBox}>
-            <Text style={styles.Boxtitle}>손익분기점 그래프</Text>
+      <Text style={styles.dateText}>{dataDate}</Text>
+      </View>
+      <View style={styles.middle_topBox}>
+            <Text style={styles.middle_Boxtitle}>손익분기점 그래프</Text>
+            <View style={{alignItems:'flex-end'}}>
+            <Text style={styles.middle_Boxsubtitle}>도달 예상<Text style={{color:'#e051ff'}}>3개월 후</Text></Text>
+            <Text style={styles.middle_BoxsubtitleDate}>2021년 9월</Text>
+            </View>
           </View>
-          <View style={{ flex: 1 }}>
+      <View style={styles.middleContainer}>
+        <View style={styles.Box}>
+          <View style={{ flex: 1 , marginTop:hp(5)}}>
           <LineChart
-                style={{ height: 200 }}
+                style={{ height: 250 }}
                 data={ data }
                 contentInset={{ top: 20, bottom: 20 }}
                 svg={{
-                    stroke: '#00BFFF',
-                    strokeWidth: 2,
+                    stroke: '#385bff',
+                    strokeWidth: 4,
                     clipPath: 'url(#clip-path-1)',
                 }}
             >
@@ -152,16 +147,20 @@ const VerticalLine = (({ x }) => (
         </View>
       </View>
       <View style={styles.bottomContainer} >
-        <View style={styles.HorizontalBox}>
-          <Text style={styles.bottomText}><View style={[styles.colorBox, { backgroundColor: 'gray' }]} />투자 원금</Text>
-          <Text style={styles.bottomText}><View style={[styles.colorBox, { backgroundColor: '#00BFFF' }]} />누적 수익(실축)</Text>
-          <Text style={styles.bottomText}><View style={[styles.colorBox, { backgroundColor: '#FFBF00' }]} />누적 수익(예측)</Text>
-          <Text style={styles.bottomText}><View style={[styles.colorBox, { backgroundColor: '#BE81F7' }]} />손익분기점</Text>
+        <View style={styles.bottomBox}>
+          <View style={styles.columnBox}>
+                <Text style={styles.bottomText}><View style={[styles.colorBox, { marginTop: wp(1),borderRadius:100,backgroundColor: "#292929" }]} />투자 원금</Text>
+                <Text style={styles.bottomText}><View style={[styles.colorBox, { marginTop: wp(1),borderRadius:100,backgroundColor: "#ffb851" }]} />누적 수익(예측)</Text>
+          </View>
+          <View style={styles.columnBox}>
+              <Text style={styles.bottomText}><View style={[styles.colorBox, { marginTop: wp(1),borderRadius:100,backgroundColor: "#385bff" }]} />누적 수익(실측)</Text>
+              <Text style={styles.bottomText}><View style={[styles.colorBox, { marginTop: wp(1),borderRadius:100,backgroundColor: "#e051ff" }]} />손익분기점</Text>
+          </View>
         </View>
       </View>
       <View style={styles.bottomContainer}>
-      <Table borderStyle={{borderWidth: 1, borderColor: '#000000'}}>
-          <Row data={HeadTable} style={styles.HeadStyle} textStyle={styles.TableText}/>
+      <Table borderStyle={{}}>
+          <Row data={HeadTable} style={styles.HeadStyle} textStyle={styles.TableTitleText}/>
           <Rows data={DataTable} textStyle={styles.TableText}/>
         </Table>
       </View>
@@ -175,62 +174,125 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "column",
-     backgroundColor: "#fff",
+    backgroundColor:"#f5f5f5"
   },
   Center:{
     justifyContent: 'center',
     alignItems: 'center',
   },
+  HeadStyle:{
+    backgroundColor:'#ebebeb',
+    borderRadius:20,
+    paddingRight:wp(3),
+    paddingTop:wp(3),
+    paddingBottom:wp(3),
+  },
+  TableTitleText:{
+    color:'#000000',
+    fontSize:wp(4),
+    fontWeight:'bold',
+    textAlign:'center'
+  },
+  TableText:{
+    color:'#000000',
+    fontSize:wp(5),
+    alignItems:'center',
+    textAlign:'center',
+    paddingTop:wp(5)
+  },
+  middle_BoxsubtitleDate:{
+    fontSize:wp(4),
+    color:'#909090',
+    paddingTop:wp(1)
+  },
+  dateText:{
+    color:'#909090',
+    fontSize:wp(4),
+    paddingLeft:wp(3.5),
+    paddingTop:wp(4),
+    paddingBottom:wp(2)
+  },
   headContainer: {
     flex: 0.30,
   },
+  topContainerWrap:{
+    backgroundColor:'#2e2e33',
+    paddingBottom:hp(3),
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius:30,
+  },
   topContainer: {
     flex: 0.8,
-    backgroundColor: "#fff",
-    padding: '1%'
+    padding: '1%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    textAlign:'center'
+    
   },
   middleContainer: {
     flex: 1,
     padding: '1%',
-    backgroundColor: "#fff",
+    backgroundColor:"#f5f5f5",
     marginBottom:'10%'
   },
   bottomContainer: {
-    flex: 0.4,
-    backgroundColor: "#fff",
-    padding: '1%'
+    flex: 0.5,
+  
+  },
+  bottomBox:{
+    width:'95%',
+    marginLeft:wp(2),
+    flexDirection: "row",
+    backgroundColor:"#ffffff",
+    padding: wp(5),
+    shadowColor: "rgba(0, 0, 0, 0.03)",
+    shadowOffset: {
+    width: 0,
+    height: 0
+    },
+  shadowRadius: 16,
+  shadowOpacity: 1,
+  borderRadius: 16,
+  justifyContent:'space-between',
+  marginBottom:hp(5)
   },
   tailContainer: {
     flex: 0.4,
     backgroundColor: "#000",
   },
-  Box: {
-    borderColor: "#000",
-    //width:"100%",
-    height: "100%",
-    borderWidth: 1,
-    margin: 'auto'
-
+  borderBox: {
+    backgroundColor:'#222225',
+    padding:wp(5),
+    borderRadius:20,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  HorizontalBox:{
-    borderColor: "#000",
-    height: "100%",
-    borderWidth: 1,
-    margin: 'auto',
-    flexDirection: 'row',
+  columnBox:{
+    flexDirection: "column",
+    justifyContent: 'space-between',
+    paddingTop:wp(2),
+  },
+  borderText:{
+    color:'#ffffff'
+  },
+  bordersubText:{
+    color:'#909090'
   },
   Boxtitle: {
-    fontSize: wp('5%'),
+    fontSize: wp(4),
     paddingLeft: wp('1%'),
     paddingBottom: wp('3%'),
     paddingTop: wp('1%'),
-    fontWeight: "bold"
+    fontWeight: "bold",
+    color:'#ffffff',
+
   },
   Boxsubtitle: {
-    fontSize: wp('3%'),
+    fontSize: wp(6),
     paddingRight: wp('1%'),
     paddingBottom: wp('3%'),
     paddingTop: wp('2%'),
+    color:'#ffffff'
 
   },
   topBox: {
@@ -242,30 +304,62 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between'
   },
   insideMiddleBox: {
-    flexDirection: 'column',
+    flexDirection: 'row',
+    justifyContent:'space-between',
   },
-  middlePriceText: {
-    fontSize: wp('5%'),
+  insideMiddleBoxWrap:{
+    marginTop:wp(7)
+  },
+  middlePriceTextYellow: {
+    fontSize: wp(6),
     fontWeight: "bold",
     paddingLeft: wp('1%'),
+    color:'#ffb851'
+  },
+  middlePriceTextBlue: {
+    fontSize: wp(6),
+    fontWeight: "bold",
+    paddingLeft: wp('1%'),
+    color:'#385bff'
   },
   middleConText: {
     paddingBottom: wp('3%'),
     paddingTop: wp('2%'),
+    color:'#ffffff'
   },
   bottomContainer: {
     flex: 0.5,
     padding: '1%'
   },
   bottomText: {
-    fontSize: wp('3%'),
-    paddingLeft: wp(2),
-    paddingTop: wp(1),
+    fontSize: wp(4),
+    fontWeight:'bold',
+    marginBottom:wp(2)
+    
   },
   colorBox: {
-    width: 10,
-    height: 10,
+    width: 15,
+    height: 15,
+    //marginRight:wp(5)
 
+  },
+  middle_topBox:{
+    backgroundColor: "#ffffff",
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius:30,
+    flexDirection: "row",
+    justifyContent: 'space-between',
+    padding:wp(5)
+    
+  },
+  middle_Boxtitle:{
+    color:'black',
+    fontSize:wp(4)
+  },
+  middle_Boxsubtitle:{
+    color:'#000000',
+    fontWeight:'bold',
+    fontSize:wp(6)
   },
 });
 export default ReportScreen;
